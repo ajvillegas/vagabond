429d3648b9991f52c73d2bc866f3f0f359ef3c0b
Alexis J. Villegas <alexis@ajvillegas.com>
2020-04-10 13:35:50 -0700

    Remove breadcrumbs tag and add column class markup

--- archive.php
+++ archive.php
@@ -20,39 +20,40 @@ get_header();
 	<main id="site-content" class="content">
 
 		<?php
-		if ( function_exists( 'breadcrumb_trail' ) ) {
-			breadcrumb_trail(
-				array(
-					'show_on_front' => false,
-					'labels'        => array(
-						'browse' => esc_html__( 'You are here:', 'vagabond' ),
-					),
-				)
-			);
-		}
-
 		if ( have_posts() ) :
 
+			if ( get_the_archive_description() ) :
+				?>
+				<header class="page-header">
+					<div class="archive-description">
+						<?php the_archive_description(); ?>
+					</div>
+				</header><!-- .page-header -->
+				<?php
+			endif;
+
 			?>
-			<header class="page-header">
-				<div class="archive-description">
-					<?php the_archive_description(); ?>
+			<div class="row">
+				<div class="col-12">
+					<div class="row">
+						<?php
+						while ( have_posts() ) :
+
+							the_post();
+
+							/*
+							 * Include the Post-Type-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', get_post_type() );
+
+						endwhile; // End of the loop.
+						?>
+					</div>
 				</div>
-			</header><!-- .page-header -->
+			</div>
 			<?php
-
-			while ( have_posts() ) :
-
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile; // End of the loop.
 
 			vagabond_archive_pagination( 'numeric' );
 
