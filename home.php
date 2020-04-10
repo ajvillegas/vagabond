ae143931fef2c300da962747dbdc83085950ee93
Alexis J. Villegas <alexis@ajvillegas.com>
2020-04-10 01:06:54 -0700

    Add blog page template

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file adds the blog page template to the theme.
+ *
+ * @package    Vagabond
+ * @author     Alexis J. Villegas
+ * @link       http://www.alexisvillegas.com
+ * @license    GPL-2.0+
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Reuse the archive.php template.
+require_once 'archive.php';
